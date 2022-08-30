17712c17f1e3e5f5fbd968c53429c3b6a7a1636c
Artyom Veremeenko <arwer13@gmail.com>
2022-08-30 10:10:39 +0300

    move configs to repo root

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 import sys
 from typing import Optional
 
-from ape import networks
+from ape import networks, accounts
 
 
-def network_name() -> Optional[str]:
+def get_network_name() -> Optional[str]:
     # It is None at moment of config.py network initialization
     if networks.active_provider is None:
         network_index = sys.argv.index("--network")
@@ -18,9 +18,16 @@ def network_name() -> Optional[str]:
     return networks.active_provider.network.name
 
 
-if network_name() in ("goerli", "goerli-fork"):
+if get_network_name() in ("goerli", "goerli-fork"):
     print(f"Using config_goerli.py addresses")
     from config_goerli import *
 else:
     print(f"Using config_mainnet.py addresses")
     from config_mainnet import *
+
+
+def get_deployer_account():
+    if "DEPLOYER" not in os.environ:
+        raise EnvironmentError("Please set DEPLOYER env variable to the deployer Ape account name")
+
+    return accounts.load(os.environ["DEPLOYER"])
