139fb4ecdea7802c987d47373f422486d16eeba8
Artyom Veremeenko <arwer13@gmail.com>
2022-08-30 08:09:54 +0300

    add capability to set network-specific config

--- tests/config.py
+++ tests/config.py
@@ -1,2 +1,26 @@
-lido_dao_agent_address = "0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c"
-dai_token_address = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+import os
+import sys
+from typing import Optional
+
+from ape import networks
+
+
+def network_name() -> Optional[str]:
+    # It is None at moment of config.py network initialization
+    if networks.active_provider is None:
+        network_index = sys.argv.index("--network")
+        if network_index + 1 < len(sys.argv):
+            network_arg = sys.argv[network_index + 1]
+            _, name, provider = network_arg.split(":")
+            return name
+        return None
+
+    return networks.active_provider.network.name
+
+
+if network_name() in ("goerli", "goerli-fork"):
+    print(f"Using config_goerli.py addresses")
+    from config_goerli import *
+else:
+    print(f"Using config_mainnet.py addresses")
+    from config_mainnet import *
---
+++ tests/config_mainnet.py
@@ -0,0 +1,2 @@
+lido_dao_agent_address = "0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c"
+dai_token_address = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
